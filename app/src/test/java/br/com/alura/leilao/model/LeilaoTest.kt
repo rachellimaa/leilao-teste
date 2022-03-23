f1886a1bf59096c79591946213d800ed72482023
Rachel Lima <rachellimaa@gmail.com>
2022-03-23 15:37:33 -0300

    fixed tests

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.kt
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.kt
@@ -101,18 +101,19 @@ class LeilaoTest {
 
     @Test
     fun treMaioresLancesComMaisDeTresLances(){
-        console.propoe(Lance(user1, 200.0))
-        console.propoe(Lance(user1, 500.0))
-        console.propoe(Lance(user1, 600.0))
-        console.propoe(Lance(user1, 700.0))
+        console.propoe(Lance(user1, 100.0))
+        console.propoe(Lance(Usuario("Teste2"), 200.0))
+        console.propoe(Lance(user1, 300.0))
         console.propoe(Lance(Usuario("Teste2"), 400.0))
+        console.propoe(Lance(user1, 500.0))
+        console.propoe(Lance(Usuario("Teste2"), 600.0))
 
         val listLances : MutableList<Lance> = console.getTresMaioresLances()
 
         assertEquals(3, listLances.size)
-        assertEquals(700.0, listLances[0].valor, delta)
-        assertEquals(600.0, listLances[1].valor, delta)
-        assertEquals(500.0, listLances[2].valor, delta)
+        assertEquals(600.0, listLances[0].valor, delta)
+        assertEquals(500.0, listLances[1].valor, delta)
+        assertEquals(400.0, listLances[2].valor, delta)
     }
 
     @Test
